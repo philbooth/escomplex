b88573034812ad12ea0e32ebe7dae34bd9a9058f
Jared Stilwell <jared.k.stilwell@gmail.com>
2016-06-22 17:44:50 -0500

    Updated unit tests to run across multiple parsers. Fixes #36.
    This commit adds support in the unit tests for running against multiple
    parsers. This brings integration issues and required options into a
    sharper focus. It will also help highlight inconsistencies across each
    parser.

--- test/index-sans-mocks.js
+++ test/index-sans-mocks.js
@@ -2,63 +2,65 @@
 
 var assert = require('chai').assert;
 var escomplex = require('../src');
-var espree = require('espree');
+var parsers = require('./helpers/parsers');
 
 suite('index parser overrides', function () {
-    test('AST callback for module does not alter behavior', function () {
-        var wasCalled = false;
-        var expected = escomplex.analyse('var a;', {});
-        var actual = escomplex.analyse(
-            'var a;',
-            {},
-            function (source) {
-                wasCalled = true;
-                return espree.parse(source, { loc: true });
-            }
-        );
-        assert.ok(wasCalled);
-        assert.deepEqual(expected, actual);
-    });
+    parsers.forEach(function (parserName, parser, options) {
+        test('AST callback for module does not alter behavior', function () {
+            var wasCalled = false;
+            var expected = escomplex.analyse('var a;', {});
+            var actual = escomplex.analyse(
+                'var a;',
+                {},
+                function (source) {
+                    wasCalled = true;
+                    return parser.parse(source, options);
+                }
+            );
+            assert.ok(wasCalled);
+            assert.deepEqual(expected, actual);
+        });
 
-    test('overriding parser fn does not alter behavior for project', function () {
-        var sources = [{
-            path: 'one',
-            source: 'var a;'
-        }, {
-            path: 'two',
-            source: 'var b;'
-        }];
-        var callCount = 0;
-        var expected = escomplex.analyse(sources, {});
-        var actual = escomplex.analyse(
-            sources,
-            {},
-            function (source) {
-                callCount++;
-                return espree.parse(source, { loc: true });
-            }
-        );
-        assert.equal(callCount, sources.length);
-        assert.deepEqual(expected, actual);
-    });
+        test('overriding parser fn does not alter behavior for project', function () {
+            var sources = [{
+                path: 'one',
+                source: 'var a;'
+            }, {
+                path: 'two',
+                source: 'var b;'
+            }];
+            var callCount = 0;
+            var expected = escomplex.analyse(sources, {});
+            var actual = escomplex.analyse(
+                sources,
+                {},
+                function (source) {
+                    callCount++;
+                    return parser.parse(source, options);
+                }
+            );
+            assert.equal(callCount, sources.length);
+            assert.deepEqual(expected, actual);
+        });
 
-    test('overriding parser options does not alter behavior', function () {
-        var sources = [{
-            path: 'one',
-            source: 'var a;'
-        }, {
-            path: 'two',
-            source: 'var b;'
-        }];
-        var expected = escomplex.analyse(sources, {});
-        var actual = escomplex.analyse(
-            sources,
-            {},
-            {
-                loc: false,
-                range: true
-            }
-        );
-        assert.deepEqual(expected, actual);
+        test('overriding parser options does not alter behavior', function () {
+            var sources = [{
+                path: 'one',
+                source: 'var a;'
+            }, {
+                path: 'two',
+                source: 'var b;'
+            }];
+            var expected = escomplex.analyse(sources, {});
+            var actual = escomplex.analyse(
+                sources,
+                {},
+                {
+                    loc: false,
+                    range: true
+                }
+            );
+            assert.deepEqual(expected, actual);
+        });
     });
 });
